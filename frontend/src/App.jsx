709ca3f577fbd39cc76a9691ea8944ce6c6e1c3c
sidisgr8 <sidatmanipal@gmail.com>
2025-02-15 20:37:02 +0530

    day 0

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Roadmap from "./components/roadmap.jsx";
+
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Linear Roadmap</h1>
+      <Roadmap />
+    </div>
+  );
+}
+
+export default App;
+
